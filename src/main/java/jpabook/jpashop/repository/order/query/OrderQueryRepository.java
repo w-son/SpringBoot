991ed46236681f7f43e69f36e48311831bdbe35a
w-son <williamson@naver.com>
2019-12-29 19:17:06 +0900

    2019/12/29 19:17

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -43,7 +43,10 @@ public class OrderQueryRepository {
                 .getResultList();
     }
 
-    /* version 5 */
+    /*
+     version 5
+     reference: https://www.objectdb.com/java/jpa/query/parameter
+    */
     public List<OrderQueryDto> findAllByDto_optimization() {
         // 쿼리 1번
         List<OrderQueryDto> result = findOrders();
@@ -53,7 +56,10 @@ public class OrderQueryRepository {
                 .map(o -> o.getOrderId())
                 .collect(Collectors.toList()); // userA의 주문번호와 userB의 주문번호가 들어가 있을것이다
 
-        // in query를 통해서 나머지 쿼리 부분을 한번에 처리했다 총 쿼리 수 1번
+        /*
+         in query를 통해서 나머지 쿼리 부분을 한번에 처리했다 총 쿼리 수 1번
+         대신 주문 아이템 별로 구분은 안된 상태로 userA와 userB의 orderItem이 들어 있을 것이다
+         */
         List<OrderItemQueryDto> orderItems = em.createQuery(
                 "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
@@ -62,10 +68,28 @@ public class OrderQueryRepository {
                 .setParameter("orderIds", orderIds)
                 .getResultList();
 
+        // 기존에 구해 놓은 orderId를 키값으로 사용하여 각각의 user에 해당하는 orderItem들을 매핑한다
         Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
                 .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
 
         result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
         return result;
+    }
+
+    /*
+     version 6
+     v5의 과정을 하나의 쿼리로 합쳐서 설계
+     하지만 일대 다 과정이 포함된 상태에서 조인을 시켜버리면 페이징 불가 + 중복이 불가피하다
+
+    */
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 }
